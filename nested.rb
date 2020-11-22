8da9ce65196bdbbeab4881e083a58bb74a1a42bc
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-21 16:39:01 -0800

    commit tree 84d52796bb661a325fd37eeb390ce8a0e4965d3b

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -15,6 +16,7 @@ def hopper
           :languages => ["C"]
         }
      }
+     programmer_hash[:grace_hopper] #mycode
 
 
 end
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for] #mycode
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join("") #mycode 
 end
 
 def adding_matz
@@ -78,9 +82,13 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+        
      }
 
-    
+#     new_programmer_hash = {yukihiro_matsumoto: {known_for: "Ruby" }}.merge(programmer_hash)
+     new_programmer_hash = {yukihiro_matsumoto: {known_for: "Ruby", languages: "LISP, C"}}.merge(programmer_hash)  #mycode, also passed with above code
+
+  
 end
 
 def changing_alan
@@ -101,7 +109,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
+
      
 end
 
@@ -121,5 +131,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 
 end
+
+
+#check notes for test suites
